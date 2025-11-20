c9592d42ec0a0e04d43ca9779aab2ff1066c2ac6
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-11-20 20:50:02 +0200

    Increased warning level and enabled warnings as errors

--- Build.lua
+++ Build.lua
@@ -2,9 +2,12 @@ workspace "HackAssembler"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
    startproject "Assembler"
+   warnings "Extra"
+   fatalwarnings "All"
 
    -- Workspace-wide build options for MSVC
    filter "system:windows"
+      flags { "MultiProcessorCompile" }
       buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
 
 OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
