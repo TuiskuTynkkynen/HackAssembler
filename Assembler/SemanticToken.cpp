fb0cdd87d6a98f4c3cd74301d13f06e83b8b9426
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-11-25 20:38:23 +0200

    Implemented SemanticToken class

---
+++ Assembler/SemanticToken.cpp
@@ -0,0 +1,126 @@
+#include "SemanticToken.h"
+
+#include <optional>
+#include <charconv>
+
+static constexpr std::expected<Operations, SemanticToken::ParseError> ParseOperation(const std::string_view token) {
+    if (token.size() != 1) {
+        return std::unexpected(SemanticToken::ParseError::InvalidTokenData);
+    }
+
+    switch (token.front()) {
+    case '=':
+        return Operations::Assignment;
+    case '!':
+        return Operations::Negation;
+    case '+':
+        return Operations::Addition;
+    case '-':
+        return Operations::Substraction;
+    case '&':
+        return Operations::BitwiseAnd;
+    case '|':
+        return Operations::BitwiseOr;
+    case ';':
+        return Operations::Jump;
+    }
+
+    assert(false); // Should never reach this
+    return std::unexpected(SemanticToken::ParseError::InvalidTokenData);
+}
+
+static constexpr std::expected<uint16_t, SemanticToken::ParseError> ParseInteger(const std::string_view token) {
+    int32_t number = 0;
+    std::from_chars_result result = std::from_chars(token.data(), token.data() + token.size(), number);
+
+    if (result.ptr != token.data() + token.size()) {
+        return std::unexpected(SemanticToken::ParseError::InvalidTokenData);
+    }
+
+    if (result.ec == std::errc::result_out_of_range || number < 0 || number > std::numeric_limits<uint16_t>::max()) {
+        return std::unexpected(SemanticToken::ParseError::IntegerOutOfRange);
+    }
+
+    return static_cast<uint16_t>(number);
+}
+
+static constexpr std::optional<Destination> TryParseRegisters(const std::string_view token) {
+    size_t registerCount = 0;
+    Destination registers{};
+
+    constexpr auto ContainsAt = [](const std::string_view str, char character, size_t index) {
+        return str.length() > index && str[index] == character;
+        };
+
+    registerCount += registers.A = ContainsAt(token, 'A', registerCount);
+
+    registerCount += registers.D = ContainsAt(token, 'D', registerCount);
+
+    registerCount += registers.M = ContainsAt(token, 'M', registerCount);
+
+    if (token.length() == registerCount) {
+        return registers;
+    }
+
+    return std::nullopt;
+}
+
+static constexpr std::optional<Jumps> TryParseJump(const std::string_view token) {
+    if (token == "JMP") return Jumps::JMP;
+    if (token == "JGT") return Jumps::JGT;
+    if (token == "JEQ") return Jumps::JEQ;
+    if (token == "JLT") return Jumps::JLT;
+    if (token == "JGE") return Jumps::JGE;
+    if (token == "JLE") return Jumps::JLE;
+    if (token == "JNE") return Jumps::JNE;
+
+    return std::nullopt;
+}
+
+static constexpr std::expected<std::string, SemanticToken::ParseError> ParseVariable(const std::string_view token) {
+    if (token.empty()) {
+        return std::unexpected(SemanticToken::ParseError::InvalidTokenData);
+    }
+
+    return std::string(token);
+}
+
+std::expected<SemanticToken, SemanticToken::ParseError> SemanticToken::Create(const Lexer::Token& token) {
+    constexpr auto construct = [](auto data) {return SemanticToken(data); };
+
+    std::optional<Destination> registers;
+    std::optional<Jumps> jump;
+
+    switch (token.Type) {
+    case Lexer::TokenType::Operation:
+        return ParseOperation(token.Data).transform(construct);
+    case Lexer::TokenType::Integer:
+        return ParseInteger(token.Data).transform(construct);
+    case Lexer::TokenType::String:
+        registers = TryParseRegisters(token.Data);
+        if (registers) return construct(registers.value());
+
+        jump = TryParseJump(token.Data);
+        if (jump) return construct(jump.value());
+
+        return ParseVariable(token.Data).transform(construct);
+    }
+
+    return std::unexpected(ParseError::InvalidTokenType);
+}
+
+static constexpr bool IsUnary(Operations operation) {
+    return operation == Operations::Negation || operation == Operations::Substraction;
+}
+
+bool SemanticToken::ValidAfter(const SemanticToken& previous) const {
+    if (previous.GetType() == Type::Jump) {
+        return false;
+    }
+
+    if (previous.GetType() == Type::Operation) {
+        return GetType() != Type::Operation || IsUnary(GetData<Operations>());
+    }
+
+    return GetType() == Type::Operation;
+}
---
+++ Assembler/SemanticToken.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include "Lexer.h"
+
+#include <string>
+#include <variant>
+#include <expected>
+
+enum class Operations : uint8_t {
+    None = 0,
+    Assignment,
+    Negation,
+    Addition,
+    Substraction,
+    BitwiseAnd,
+    BitwiseOr,
+    Jump,
+};
+
+enum class Jumps : uint8_t {
+    None = 0,
+    JMP,
+    JGT,
+    JEQ,
+    JLT,
+    JGE,
+    JLE,
+    JNE,
+};
+
+struct Destination {
+    bool A : 1 = false;
+    bool D : 1 = false;
+    bool M : 1 = false;
+};
+
+class SemanticToken {
+public:
+    enum class Type : uint8_t {
+        Registers,
+        Integer,
+        Variable,
+        Jump,
+        Operation,
+    };
+
+    enum class ParseError {
+        Unexpected = 0,
+        InvalidTokenType,
+        InvalidTokenData,
+        IntegerOutOfRange,
+    };
+    static std::expected<SemanticToken, ParseError> Create(const Lexer::Token& token);
+
+    bool ValidAfter(const SemanticToken& previous) const;
+    inline constexpr Type GetType() const { return static_cast<Type>(m_Data.index()); };
+    
+    template <typename T>
+    inline constexpr const T& GetData() const { return std::get<T>(m_Data); };
+    template <typename T>
+    inline constexpr const T* TryGetData() const { return std::get_if<T>(&m_Data); };
+    template <typename T>
+    inline constexpr bool HasValue(const T& value) const {
+        const T* ptr = TryGetData<T>();
+        return ptr && *ptr == value;
+    }
+private:
+    SemanticToken(auto data) : m_Data(data) {}
+    std::variant<Destination, uint16_t, std::string, Jumps, Operations> m_Data;
+};
+
