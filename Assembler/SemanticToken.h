ae8a352cde86cad4f73277f2d7b369005269f932
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-11-30 18:51:05 +0200

    Updated Jumps enum to match machine code values

--- Assembler/SemanticToken.h
+++ Assembler/SemanticToken.h
@@ -18,14 +18,14 @@ enum class Operations : uint8_t {
 };
 
 enum class Jumps : uint8_t {
-    None = 0,
-    JMP,
-    JGT,
-    JEQ,
-    JLT,
-    JGE,
-    JLE,
-    JNE,
+    None = 0b000,
+    JGT = 0b001,
+    JEQ = 0b010,
+    JGE = 0b011,
+    JLT = 0b100,
+    JNE = 0b101,
+    JLE = 0b110,
+    JMP = 0b111,
 };
 
 struct Destination {
