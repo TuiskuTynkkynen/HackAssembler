0d516f4ed1e638af55a32ebf81dbd3405bb98866
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2025-12-02 22:02:13 +0200

    Implemented File IO functions

---
+++ Assembler/IO/File.h
@@ -0,0 +1,23 @@
+#include <fstream>
+#include <optional>
+#include <filesystem>
+
+namespace IO {
+    [[nodiscard]] std::optional<std::ifstream> TryOpenFileInput(const std::string& fileName) {
+        std::ifstream file;
+        file.open(fileName);
+        
+        return file.good() ? std::optional<std::ifstream>(std::move(file)) : std::optional<std::ifstream>(std::nullopt);
+    }
+    
+    [[nodiscard]] std::optional<std::ofstream> TryOpenFileOutput(const std::string& fileName) {
+        std::ofstream file;
+        file.open(fileName);
+        
+        return file.good() ? std::optional<std::ofstream>(std::move(file)) : std::optional<std::ofstream>(std::nullopt);
+    }
+
+    [[nodiscard]] std::string GetAblsolutePath(std::string_view path) {
+        return std::filesystem::absolute(path).string();
+    }
+}
